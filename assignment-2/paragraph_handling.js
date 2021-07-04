dcdaafb236471f5961ad586a3e00f119b0969935
sumeetgodse <sumeetgodse1997@gmail.com>
2021-07-04 15:47:33 +0530

    Modified Mobile No. regexp

--- assignment-2/paragraph_handling.js
+++ assignment-2/paragraph_handling.js
@@ -4,7 +4,7 @@
  */
 const regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/g;
 const regexPhoneno = /^[(]?[\+]?[0-9]{2}[)]?[-\s\.]?[0-9]{5}[-\s\.]?[0-9]{5}$/g;
-const regexMobile = /^[0-9]{10}$/g;
+const regexMobile = /^\+?(?:[0-9] ?){6,14}[0-9]$/g;
 
 const paragraph = "Lorem ipsum dolor 9221122108 sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor sed viverra ipsum nunc aliquet bibendum enim. In massa tempor nec feugiat. Nunc aliquet bibendum enim facilisis gravida. mytraining@deqode.com Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Amet luctus venenatis lectus magna fringilla. Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque in. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. +91-20200-21210 Sagittis orci a scelerisque purus semper eget duis. Nulla pharetra diam sit amet nisl suscipit. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Fusce (+91)-20200-21210 ut placerat mt@test.inc orci nulla. Pharetra vel turpis nunc eget lorem dolor. Tristique senectus et netus et malesuada.";
 
