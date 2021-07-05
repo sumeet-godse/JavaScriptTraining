d0792818f3bc76be0f9a8ca058e036a7d3de4b7f
sumeetgodse <sumeetgodse1997@gmail.com>
2021-07-05 09:10:31 +0530

    Problem 4 solution updated

--- assignment-3/problem-4.js
+++ assignment-3/problem-4.js
@@ -3,42 +3,25 @@
  * We need to modify it using promises to print sequentially 0 to 10. For example, if 0 takes 6 seconds to print
  * and 1 takes two seconds to print, then 1 should wait for 0 to print, and so on.
  */
-let outputArray = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
-//let randomNumbers = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1];
-let index = 0;
-
+// For loop which prints 0 to 10 at irregular intervals
 for(let i = 0; i <= 10; i++) {
     let randomnumber = Math.floor(Math.random() * (6 - 0 + 1));
     setTimeout(() => {
-        outputArray[i] = i;
-        //randomNumbers[i]=randomnumber
         console.log("Printing " + i + " after " + randomnumber + " seconds.");
     }, randomnumber * 1000);
 }
 
-for(let i = 0; i <= 10; i ++) {
-    setTimeout(() => {
-        processItem()
-        .then((message) => {
-            console.log(message);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-    }, 6000);
+// Our solution using promise.all
+let promises = [];
+
+for(let i = 0; i < 11; i ++) {
+    let randomNumber = Math.floor(Math.random() * (6 - 0 + 1));
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Printing " + i + " after " + randomNumber + " seconds");
+        }, randomNumber*1000);
+    });
+    promises.push(promise);
 }
 
-function processItem() {
-    return new Promise((resolve, reject) => {
-        const element =  outputArray[index];
-        if(element != -1) {
-            index ++;
-            resolve(element);
-        } else {
-            reject("waiting");
-        }
-    });
-};
-
-
-
+Promise.all(promises).then((message) => console.log(message));
